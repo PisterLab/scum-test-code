a48859e48527dc3f9b0fe7b98d25e9b875ac0d63
unknown <brian.kilberg@gmail.com>
2019-09-26 15:02:00 -0700

    refactored into a separate file

---
+++ lighthouse.d
@@ -0,0 +1,10 @@
+.\lighthouse.o: lighthouse.c
+.\lighthouse.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
+.\lighthouse.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\time.h
+.\lighthouse.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\rt_misc.h
+.\lighthouse.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
+.\lighthouse.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\lighthouse.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
+.\lighthouse.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\math.h
+.\lighthouse.o: scum_radio_bsp.h
+.\lighthouse.o: lighthouse.h
---
+++ lighthouse.h
@@ -0,0 +1,8 @@
+typedef enum pulse_type_t{AZ=0,AZ_SKIP=1,EL=2,EL_SKIP=3,LASER=4,INVALID = 5} pulse_type_t;
+
+//functions
+pulse_type_t classify_pulse(unsigned int timestamp_rise, unsigned int timestamp_fall);
+void update_state(pulse_type_t pulse_type, unsigned int timestamp_rise);
+void azimuth_state(pulse_type_t pulse_type, unsigned int timestamp_rise);
+void update_state_short(pulse_type_t pulse_type, unsigned int timestamp_rise);
+unsigned int sync_pulse_width_compensate(unsigned int pulse_width);
