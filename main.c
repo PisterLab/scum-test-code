29269ee9d2d48b9c8584323789b041584ecb3b37
unknown <brian.kilberg@gmail.com>
2019-10-07 18:58:58 -0700

    narrowed freq sweep range

--- main.c
+++ main.c
@@ -89,7 +89,7 @@ void test_LC_sweep_tx(void) {
 	radio_txEnable();
 	
 	while (1) {
-		for (coarse=20; coarse<30; coarse++) {
+		for (coarse=22; coarse<25; coarse++) {
 			for (mid=0; mid<32; mid++) {
 				for (fine=0; fine<32; fine++) {
 					// Construct the packet 
