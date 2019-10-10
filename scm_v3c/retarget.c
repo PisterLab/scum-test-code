5978ddea7f24c79a8b67b716334d34f013709f7d
changtengfei <tengfei.chang@gmail.com>
2019-10-10 18:58:12 +0200

    SW-5. format retarget.c

--- scm_v3c/retarget.c
+++ scm_v3c/retarget.c
@@ -7,55 +7,47 @@
 #pragma import(__use_no_semihosting)
 
 struct __FILE { 
-	unsigned char * ptr;
-	};
+    unsigned char * ptr;
+};
 
-FILE __stdout = 	{(unsigned char *)	APB_UART_BASE};
-FILE __stdin = 		{(unsigned char *)	APB_UART_BASE};
+FILE __stdout =     {(unsigned char *)    APB_UART_BASE};
+FILE __stdin  =     {(unsigned char *)    APB_UART_BASE};
 
-int fputc(int ch, FILE *f)
-{
-  return(uart_out(ch));
-   
+int fputc(int ch, FILE *f) {
+    return(uart_out(ch));
 }
 
-int fgetc(FILE *f)
-{ 
- 	return(uart_in());
+int fgetc(FILE *f) { 
+     return(uart_in());
 }
 
-int ferror(FILE *f)
-{
-  return 0; 
+int ferror(FILE *f) {
+    return 0; 
 }
 
-int uart_out(int ch)
-{
-	unsigned char* UARTPtr;
-	UARTPtr = (unsigned char*)APB_UART_BASE;
-	*UARTPtr = (char)ch;
-	return(ch);
+int uart_out(int ch) {
+    unsigned char* UARTPtr;
+    UARTPtr = (unsigned char*)APB_UART_BASE;
+    *UARTPtr = (char)ch;
+    return(ch);
 }
 
-int uart_in()
-{
-	char ch;
-	unsigned char* UARTPtr;
-	UARTPtr = (unsigned char*)APB_UART_BASE;	
-	ch = *UARTPtr;
-	uart_out(ch);
-	return((int)ch);
+int uart_in() {
+    char ch;
+    unsigned char* UARTPtr;
+    UARTPtr = (unsigned char*)APB_UART_BASE;    
+    ch = *UARTPtr;
+    uart_out(ch);
+    return((int)ch);
 }
 
-void _ttywrch(int ch)
-{ 
-  fputc(ch,&__stdout); 
+void _ttywrch(int ch) { 
+    fputc(ch,&__stdout); 
 }
 
 void _sys_exit(void) {
 
-	printf("\r\nTEST DONE\r\n");
-	while(1); 
-	
+    printf("\r\nTEST DONE\r\n");
+    while(1); 
 }
 
