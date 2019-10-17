a766edd233d516512bf053a1881bbae70aea94d3
changtengfei <tengfei.chang@gmail.com>
2019-10-17 15:22:17 +0200

    SW-11. format cm0dsasm.s

--- scm_v3c/cm0dsasm.s
+++ scm_v3c/cm0dsasm.s
@@ -32,40 +32,40 @@ __Vectors               DCD        __initial_sp
                         DCD        0
                         DCD        0
                         DCD        0
-                        DCD     0
                         DCD        0
                         DCD        0
-                        DCD     0
+                        DCD        0
+                        DCD        0
                         DCD        0
                         
                         ; External Interrupts
-                                                
+                        
                         DCD        UART_Handler
-                        DCD        interrupt_gpio3_activehigh_debounced
-                        DCD        optical_irq_in
+                        DCD        EXT_GPIO3_ACTIVEHIGH_DEBOUNCED_Handler
+                        DCD        EXT_OPTICAL_IRQ_IN_Handler
                         DCD        ADC_Handler
                         DCD        0
                         DCD        0
                         DCD        RF_Handler
                         DCD        RFTIMER_Handler
-                        DCD        interrupt_rawchips_startval
-                        DCD        interrupt_rawchips_32
+                        DCD        RAWCHIPS_STARTVAL_Handler
+                        DCD        RAWCHIPS_32_Handler
                         DCD        0
-                        DCD        optical_sfd_interrupt
-                        DCD        interrupt_gpio8_activehigh
-                        DCD        interrupt_gpio9_activelow
-                        DCD        interrupt_gpio10_activelow
+                        DCD        OPTICAL_SFD_Handler
+                        DCD        EXT_GPIO8_ACTIVEHIGH_Handler
+                        DCD        EXT_GPIO9_ACTIVELOW_Handler
+                        DCD        EXT_GPIO10_ACTIVELOW
                         DCD        0 
               
                 AREA |.text|, CODE, READONLY
 ;Interrupt Handlers
-Reset_Handler   PROC
-        GLOBAL Reset_Handler
+Reset_Handler       PROC
+        GLOBAL      Reset_Handler
         ENTRY
         
-        LDR     R1, =0xE000E100         ;Interrupt Set Enable Register
-        LDR     R0, =0x0000             ;<- REMEMBER TO ENABLE THE INTERRUPTS!!
-        STR     R0, [R1]
+        LDR         R1, =0xE000E100         ;Interrupt Set Enable Register
+        LDR         R0, =0x0000             ;<- REMEMBER TO ENABLE THE INTERRUPTS!!
+        STR         R0, [R1]
         
         ;IP wake up just to solve interrupts
         ; LDR r0, =0xE000ED10; System Control Register address
@@ -74,185 +74,182 @@ Reset_Handler   PROC
         ; ORRS r1, r2; Set SLEEPONEXIT bit
         ; STR r1, [r0]
         
-        IMPORT  __main
-        LDR     R0, =__main               
-        BX      R0                        ;Branch to __main
-                ENDP
-    
-    
-UART_Handler    PROC
-        EXPORT UART_Handler
-        IMPORT UART_ISR
-        
-        PUSH    {R0,LR}
-        
-        MOVS R0, #1 ;         ;MASK all interrupts
-        MSR PRIMASK, R0 ;         
-        
-        BL UART_ISR
-
-                
-        MOVS    R0, #0        ;ENABLE all interrupts
-        MSR    PRIMASK, R0
-        
-        POP     {R0,PC}
+        IMPORT      __main
+        LDR         R0, =__main               
+        BX          R0                        ;Branch to __main
         ENDP
+
+UART_Handler        PROC
+        EXPORT      UART_Handler
+        IMPORT      UART_ISR
         
-ADC_Handler    PROC
-        EXPORT     ADC_Handler
-        IMPORT    ADC_ISR
+        PUSH        {R0,LR}
         
-        PUSH    {R0,LR}
+        MOVS        R0, #1 ;         ;MASK all interrupts
+        MSR         PRIMASK, R0 ;         
         
-        MOVS     R0, #1 ;         ;MASK all interrupts
-        MSR     PRIMASK, R0 ; 
+        BL          UART_ISR
+        
+                
+        MOVS        R0, #0        ;ENABLE all interrupts
+        MSR         PRIMASK, R0
+        
+        POP         {R0,PC}
+        ENDP
+
+ADC_Handler         PROC
+        EXPORT      ADC_Handler
+        IMPORT      ADC_ISR
+        
+        PUSH        {R0,LR}
+        
+        MOVS        R0, #1 ;         ;MASK all interrupts
+        MSR         PRIMASK, R0 ; 
         ;STR        R0,[R1]    
         
-        BL     ADC_ISR
+        BL          ADC_ISR
         
-        MOVS    R0, #0        ;ENABLE all interrupts
-        MSR    PRIMASK, R0
+        MOVS        R0, #0        ;ENABLE all interrupts
+        MSR         PRIMASK, R0
         
-        POP    {R0,PC}
-        
+        POP         {R0,PC}
         ENDP
 
 RF_Handler    PROC
-        EXPORT     RF_Handler
-        IMPORT    RF_ISR
+        EXPORT      RF_Handler
+        IMPORT      RF_ISR
         
-        PUSH    {R0,LR}
+        PUSH        {R0,LR}
         
-        MOVS     R0, #1 ;         ;MASK all interrupts
-        MSR     PRIMASK, R0 ; 
+        MOVS        R0, #1 ;         ;MASK all interrupts
+        MSR         PRIMASK, R0 ; 
         ;STR        R0,[R1]    
         
-        BL     RF_ISR
+        BL          RF_ISR
         
-        MOVS    R0, #0        ;ENABLE all interrupts
-        MSR    PRIMASK, R0
+        MOVS        R0, #0        ;ENABLE all interrupts
+        MSR         PRIMASK, R0
         
-        POP    {R0,PC}
+        POP         {R0,PC}
         ENDP
         
 RFTIMER_Handler    PROC
         EXPORT     RFTIMER_Handler
-        IMPORT    RFTIMER_ISR
+        IMPORT      RFTIMER_ISR
         
-        PUSH    {R0,LR}
+        PUSH        {R0,LR}
         
-        MOVS     R0, #1 ;         ;MASK all interrupts
-        MSR     PRIMASK, R0 ; 
+        MOVS        R0, #1 ;         ;MASK all interrupts
+        MSR         PRIMASK, R0 ; 
         ;STR        R0,[R1]    
         
-        BL     RFTIMER_ISR
+        BL          RFTIMER_ISR
         
-        MOVS    R0, #0        ;ENABLE all interrupts
-        MSR    PRIMASK, R0
+        MOVS        R0, #0        ;ENABLE all interrupts
+        MSR         PRIMASK, R0
         
-        POP    {R0,PC}
+        POP         {R0,PC}
         ENDP
 
 ; -----------------------------
 ; BEGIN EXTERNAL INTERRUPT ISRS
 ; -----------------------------
-interrupt_gpio3_activehigh_debounced    PROC
-        EXPORT     interrupt_gpio3_activehigh_debounced
-        IMPORT    INTERRUPT_GPIO3_ISR
+EXT_GPIO3_ACTIVEHIGH_DEBOUNCED_Handler    PROC
+        EXPORT      EXT_GPIO3_ACTIVEHIGH_DEBOUNCED_Handler
+        IMPORT      INTERRUPT_GPIO3_ISR
         
-        PUSH    {R0,LR}
+        PUSH        {R0,LR}
         
-        MOVS     R0, #1 ;         ;MASK all interrupts
-        MSR     PRIMASK, R0 ; 
+        MOVS        R0, #1 ;         ;MASK all interrupts
+        MSR         PRIMASK, R0 ; 
         ;STR        R0,[R1]    
         
-        BL     INTERRUPT_GPIO3_ISR
+        BL          INTERRUPT_GPIO3_ISR
         
-        MOVS    R0, #0        ;ENABLE all interrupts
-        MSR    PRIMASK, R0
+        MOVS        R0, #0        ;ENABLE all interrupts
+        MSR         PRIMASK, R0
         
-        POP    {R0,PC}
+        POP         {R0,PC}
         ENDP
 
-interrupt_gpio8_activehigh    PROC
-        EXPORT     interrupt_gpio8_activehigh
-        IMPORT    INTERRUPT_GPIO8_ISR
+EXT_GPIO8_ACTIVEHIGH_Handler    PROC
+        EXPORT      EXT_GPIO8_ACTIVEHIGH_Handler
+        IMPORT      INTERRUPT_GPIO8_ISR
         
-        PUSH    {R0,LR}
+        PUSH        {R0,LR}
         
-        MOVS     R0, #1 ;         ;MASK all interrupts
-        MSR     PRIMASK, R0 ; 
+        MOVS        R0, #1 ;         ;MASK all interrupts
+        MSR         PRIMASK, R0 ; 
         ;STR        R0,[R1]    
         
-        BL     INTERRUPT_GPIO8_ISR
+        BL          INTERRUPT_GPIO8_ISR
         
-        MOVS    R0, #0        ;ENABLE all interrupts
-        MSR    PRIMASK, R0
+        MOVS        R0, #0        ;ENABLE all interrupts
+        MSR         PRIMASK, R0
         
-        POP    {R0,PC}
+        POP         {R0,PC}
         ENDP
 
-interrupt_gpio9_activelow    PROC
-        EXPORT     interrupt_gpio9_activelow
-        IMPORT    INTERRUPT_GPIO9_ISR
+EXT_GPIO9_ACTIVELOW_Handler    PROC
+        EXPORT      EXT_GPIO9_ACTIVELOW_Handler
+        IMPORT      INTERRUPT_GPIO9_ISR
         
-        PUSH    {R0,LR}
+        PUSH        {R0,LR}
         
-        MOVS     R0, #1 ;         ;MASK all interrupts
-        MSR     PRIMASK, R0 ; 
+        MOVS        R0, #1 ;         ;MASK all interrupts
+        MSR         PRIMASK, R0 ; 
         ;STR        R0,[R1]    
         
-        BL     INTERRUPT_GPIO9_ISR
+        BL          INTERRUPT_GPIO9_ISR
         
-        MOVS    R0, #0        ;ENABLE all interrupts
-        MSR    PRIMASK, R0
+        MOVS        R0, #0        ;ENABLE all interrupts
+        MSR         PRIMASK, R0
         
-        POP    {R0,PC}
+        POP         {R0,PC}
         ENDP
 
-interrupt_gpio10_activelow    PROC
-        EXPORT     interrupt_gpio10_activelow
-        IMPORT    INTERRUPT_GPIO10_ISR
+EXT_GPIO10_ACTIVELOW    PROC
+        EXPORT      EXT_GPIO10_ACTIVELOW
+        IMPORT      INTERRUPT_GPIO10_ISR
         
-        PUSH    {R0,LR}
+        PUSH        {R0,LR}
         
-        MOVS     R0, #1 ;         ;MASK all interrupts
-        MSR     PRIMASK, R0 ; 
+        MOVS        R0, #1 ;         ;MASK all interrupts
+        MSR         PRIMASK, R0 ; 
         ;STR        R0,[R1]    
         
-        BL     INTERRUPT_GPIO10_ISR
+        BL          INTERRUPT_GPIO10_ISR
         
-        MOVS    R0, #0        ;ENABLE all interrupts
-        MSR    PRIMASK, R0
+        MOVS        R0, #0        ;ENABLE all interrupts
+        MSR         PRIMASK, R0
         
-        POP    {R0,PC}
+        POP         {R0,PC}
         ENDP
 
 ; ---------------------------
 ; END EXTERNAL INTERRUPT ISRS
 ; ---------------------------
 
-interrupt_rawchips_startval    PROC
-        EXPORT     interrupt_rawchips_startval
-        IMPORT    RAWCHIPS_STARTVAL_ISR
+RAWCHIPS_STARTVAL_Handler    PROC
+        EXPORT      RAWCHIPS_STARTVAL_Handler
+        IMPORT      RAWCHIPS_STARTVAL_ISR
         
-        PUSH    {R0,LR}
+        PUSH        {R0,LR}
         
-        MOVS     R0, #1 ;         ;MASK all interrupts
-        MSR     PRIMASK, R0 ; 
+        MOVS        R0, #1 ;         ;MASK all interrupts
+        MSR         PRIMASK, R0 ; 
         ;STR        R0,[R1]    
         
-        BL     RAWCHIPS_STARTVAL_ISR
+        BL          RAWCHIPS_STARTVAL_ISR
         
-        MOVS    R0, #0        ;ENABLE all interrupts
-        MSR    PRIMASK, R0
+        MOVS        R0, #0        ;ENABLE all interrupts
+        MSR         PRIMASK, R0
         
-        POP    {R0,PC}
-        
+        POP         {R0,PC}
         ENDP
 
-interrupt_rawchips_32    PROC
-        EXPORT      interrupt_rawchips_32
+RAWCHIPS_32_Handler    PROC
+        EXPORT      RAWCHIPS_32_Handler
         IMPORT      RAWCHIPS_32_ISR
         
         PUSH        {R0,LR}
@@ -269,8 +266,8 @@ interrupt_rawchips_32    PROC
         POP         {R0,PC}
         ENDP
 
-optical_irq_in      PROC
-        EXPORT      optical_irq_in
+EXT_OPTICAL_IRQ_IN_Handler      PROC
+        EXPORT      EXT_OPTICAL_IRQ_IN_Handler
         IMPORT      OPTICAL_32_ISR
         
         PUSH        {R0,LR}
@@ -287,8 +284,8 @@ optical_irq_in      PROC
         POP         {R0,PC}
         ENDP
 
-optical_sfd_interrupt    PROC
-        EXPORT      optical_sfd_interrupt
+OPTICAL_SFD_Handler    PROC
+        EXPORT      OPTICAL_SFD_Handler
         IMPORT      OPTICAL_SFD_ISR
         
         PUSH        {R0,LR}
