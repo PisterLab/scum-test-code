7d243bde1471dd1215510844e341f4dca5d550c2
changtengfei <tengfei.chang@gmail.com>
2019-10-29 18:28:10 +0100

    SW-12. update freq_sweep_tx test scripts.

--- tests/test_freq_sweep_tx.py
+++ tests/test_freq_sweep_tx.py
@@ -1,50 +1,12 @@
 import pytest
 import os
 import time
-import serial
-import threading
 
 # =========================== variables =======================================
 
-BAUDRATE_OPENMOTE           = 115200
-BAUDRATE_SCUM               = 19200
+LOG_FILE                    = 'freq_sweep_output.txt'
+TARGET_STRING               = 'Ptest'
 
-pytest.output_openmote      = ''
-pytest.output_scum          = ''
-
-EXPERIEMNT_DURATION         = 491 # second
-
-# =========================== class ===========================================
-
-class serialReader(threading.Thread):
-
-    def __init__(self,serialport=None, baudrate=None):
-        
-        self.goOn       = True
-        
-        self.serialport = serialport
-        self.baudrate   = baudrate
-        
-        self.output     = ''
-        
-        threading.Thread.__init__(self)
-        
-    def run(self):
-        
-        self.serial = serial.Serial(self.serialport,self.baudrate,timeout=1)
-        
-        while self.goOn:
-            output = self.serial.read(100)
-            self.output += str(output)
-                
-        self.serial.close()
-        
-    def close(self):
-        self.goOn = False
-        
-    def get_output(self):
-        return self.output
-        
 # =========================== test ============================================
 
 def syscall(cmd):
@@ -73,29 +35,19 @@ def test_compilation():
 def test_bootload(serial_openmote, serial_scum):
     syscall("echo bootload...")
     
-    # starting logging the serial output
-    serial_openmote.start()
-    serial_scum.start()
-    
     result = syscall("python scm_v3c\\bootload\\bootload.py -tp %PORT_TEENSY% -i scm_v3c\\applications\\freq_sweep_tx_test\\freq_sweep_tx_test.bin")
     assert result == None
-    
-    # waiting to until the test ends
-    time.sleep(EXPERIEMNT_DURATION)
-    serial_openmote.close()
-    serial_scum.close()
-    
-    pytest.output_openmote  = serial_openmote.get_output()
-    pytest.output_scum      = serial_scum.get_output()
-    
-    # use pytest -s to see the print out message
-    print   'openmote_output\n', pytest.output_openmote
-    print   'scum_output\n', pytest.output_scum
-    
 
 def test_communication():
-    syscall("echo communication...")
+    syscall("echo frequency_sweep_test...")
 
-    assert 'Ptest' in pytest.output_openmote
+    result = syscall("python scm_v3c\\applications\\freq_sweep_tx_test\\freq_sweep_tx_logger.py")
+    assert result == None
     
-    syscall("echo Frames sent by SCuM are received correctly by OpenMote.")
+    with open(LOG_FILE,'rb') as f:
+        assert TARGET_STRING in f.read()
+        
+    result = syscall("python scm_v3c\\applications\\freq_sweep_tx_test\\freq_sweep_parser.py")
+    assert result == None
+    
+    syscall("echo Frequency Sweep Test complete!")
