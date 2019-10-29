6259b475576e5473e6ce513feaff795793aa1d33
changtengfei <tengfei.chang@gmail.com>
2019-10-29 16:33:51 +0100

    SW-12. update parser scripts to support raw_data with and withou rssi reading values.

--- scm_v3c/applications/freq_sweep_tx_test/freq_sweep_parser.py
+++ scm_v3c/applications/freq_sweep_tx_test/freq_sweep_parser.py
@@ -8,6 +8,7 @@ matplotlib.rcParams.update({'font.size': 12})
 # =========================== variables =======================================
 
 LOG_FILE_START                    = 'freq_sweep_output'
+RSSI_READ_INDICATOR               = 'with_rssi'
 
 
 NUM_PKT_PER_SETTING         = 3
@@ -45,17 +46,22 @@ def converter_config_linear_2_text(linear_configure):
     output = "{0}.{1}.{2}".format(coarse, middle, fine)
     return output
     
-def converter_config_text_2_linear(text_config):
+def converter_config_text_2_linear(text_config, has_rssi):
     linear_config = None
     freq_offset   = None
+    rssi          = None
     try:
-        ptest, coarse, middle, fine = text_config.split('.')
+        if has_rssi:
+            ptest, coarse, middle, fine, rssi = text_config.split('.')
+            assert int(rssi)>-127
+        else:
+            ptest, coarse, middle, fine = text_config.split('.')
         assert int(coarse) < 32 and int(middle) < 32 and int(fine) < 32
         linear_config = (int(coarse) << 10) | (int(middle) << 5) | int(fine)
         freq_offset   = int(ptest.split(' ')[0])
     except:
         pass
-    return freq_offset, linear_config
+    return freq_offset, linear_config, rssi
     
 # ==== tests
 
@@ -66,48 +72,72 @@ if __name__ == '__main__':
 
     for file in os.listdir("./"):
         if file.startswith(LOG_FILE_START) and file.endswith('.txt'):
-            data[file] = {
-                'freq_offset':  [],
-                'linear_config':[]
-            }
+            has_rssi = False
+            if RSSI_READ_INDICATOR in file:
+                has_rssi = True
+                data[file] = {
+                    'freq_offset':  [],
+                    'linear_config':[],
+                    'rssi':[]
+                }
+            else:
+                data[file] = {
+                    'freq_offset':  [],
+                    'linear_config':[]
+                }
+            
+            print "parsing {0}...".format(file)
+            
             with open(file,'rb') as lf:
                 for line in lf:
                     index = line.find('Ptest.')
                     if index != -1:
-                        freq_offset, linear_config = converter_config_text_2_linear(line)
+                        freq_offset, linear_config, rssi = converter_config_text_2_linear(line, has_rssi)
                         if freq_offset != None and linear_config != None:
                             data[file]['freq_offset'].append(freq_offset)
                             data[file]['linear_config'].append(linear_config)
-                
+                            if rssi != None:
+                                data[file]['rssi'].append(rssi)
+                            
     for key, item in data.items():
-        fig, ax = plt.subplots(figsize=(16, 4))
-        ax.scatter(
-            item['linear_config'], item['freq_offset'],
-            label="channel {0}".format(key.split('_')[-1].split('.')[0])
-        )
-    
-        ax.set_ylabel('FREQOFFSET (step 7800Hz)')
-        ax.set_xlabel('coarse.middle.fine')
         
+        for item_key in item:
+            fig, ax = plt.subplots(figsize=(16, 4))
+            
+            if item_key == 'linear_config':
+                # pass the linear_config, which is the x_axis
+                continue
+            
+            ax.scatter(
+                item['linear_config'], item[item_key],
+                label="channel {0}".format(key.split('_')[-1].split('.')[0])
+            )
         
-        x_s_lim   = 0
-        x_e_lim   = NUM_CONFIG
-        SCALE     = 1000
-        
-        # x_s_lim   = 23*32*32
-        # x_e_lim   = 29*32*32
-        # SCALE     = 200
-        
-        ax.set_xlim(x_s_lim,x_e_lim)
-        ax.set_ylim(-60,60)
-        
-        xticks = [x_s_lim+i*SCALE for i in range((x_e_lim-x_s_lim)/SCALE)]
-        xlabel = [converter_config_linear_2_text(i) for i in xticks]
-        
-        ax.set_xticks(xticks)
-        ax.set_xticklabels(xlabel,rotation = 45)
-        
-        ax.legend(markerscale=0.7, scatterpoints=1)
-        ax.grid(True)
-        plt.tight_layout()
-        plt.savefig("{0}.png".format(key.split('.')[0]))
+            ax.set_ylabel(item_key)
+            ax.set_xlabel('coarse.middle.fine')
+            
+            x_s_lim   = 0
+            x_e_lim   = NUM_CONFIG
+            SCALE     = 1000
+            
+            # x_s_lim   = 23*32*32
+            # x_e_lim   = 29*32*32
+            # SCALE     = 200
+            
+            ax.set_xlim(x_s_lim,x_e_lim)
+            
+            if item_key == 'freq_offset':
+                ax.set_ylim(-60,60)
+            elif item_key == 'rssi':
+                ax.set_ylim(-120,0)
+            
+            xticks = [x_s_lim+i*SCALE for i in range((x_e_lim-x_s_lim)/SCALE)]
+            xlabel = [converter_config_linear_2_text(i) for i in xticks]
+            
+            ax.set_xticks(xticks)
+            ax.set_xticklabels(xlabel,rotation = 45)
+            
+            ax.legend(markerscale=0.7, scatterpoints=1)
+            ax.grid(True)
+            plt.tight_layout()
+            plt.savefig("{0}_{1}.png".format(key.split('.')[0],item_key))
