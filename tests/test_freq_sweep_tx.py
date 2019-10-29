294c39afc758cca33fcb25162705e1bdd8d8e674
changtengfei <tengfei.chang@gmail.com>
2019-10-29 18:44:07 +0100

    SW-12. update the test scripts

--- tests/test_freq_sweep_tx.py
+++ tests/test_freq_sweep_tx.py
@@ -29,25 +29,25 @@ def serial_scum():
 
 def test_compilation():
     syscall("echo compilation...")
-    result = syscall("%KEIL_UV_DIR%\\UV4.exe -b scm_v3c\\applications\\freq_sweep_tx_test\\freq_sweep_tx_test.uvprojx")
+    result = syscall("%KEIL_UV_DIR%\\UV4.exe -b scm_v3c\\applications\\freq_sweep_tx\\freq_sweep_tx.uvprojx")
     assert result == None
     
 def test_bootload(serial_openmote, serial_scum):
     syscall("echo bootload...")
     
-    result = syscall("python scm_v3c\\bootload\\bootload.py -tp %PORT_TEENSY% -i scm_v3c\\applications\\freq_sweep_tx_test\\freq_sweep_tx_test.bin")
+    result = syscall("python scm_v3c\\bootload\\bootload.py -tp %PORT_TEENSY% -i scm_v3c\\applications\\freq_sweep_tx\\freq_sweep_tx.bin")
     assert result == None
 
 def test_communication():
     syscall("echo frequency_sweep_test...")
 
-    result = syscall("python scm_v3c\\applications\\freq_sweep_tx_test\\freq_sweep_tx_logger.py")
+    result = syscall("python scm_v3c\\applications\\freq_sweep_tx\\freq_sweep_tx_logger.py")
     assert result == None
     
     with open(LOG_FILE,'rb') as f:
         assert TARGET_STRING in f.read()
         
-    result = syscall("python scm_v3c\\applications\\freq_sweep_tx_test\\freq_sweep_parser.py")
+    result = syscall("python scm_v3c\\applications\\freq_sweep_tx\\freq_sweep_parser.py")
     assert result == None
     
     syscall("echo Frequency Sweep Test complete!")
