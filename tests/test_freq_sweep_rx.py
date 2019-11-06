5341f80a72bd558c2657cac4098ec1ecc34465cd
changtengfei <tengfei.chang@gmail.com>
2019-11-06 11:49:27 +0100

    SW-13. allow return code equal to 1 (warning only)

--- tests/test_freq_sweep_rx.py
+++ tests/test_freq_sweep_rx.py
@@ -13,9 +13,23 @@ def syscall(cmd):
 # ==== tests
 
 def test_compilation():
+    '''
+        using the UV4.exe executor to build the project. Pass when return code <=1
+        ERRORLEVEL    Description
+        0     No Errors or Warnings
+        1     Warnings Only
+        2     Errors
+        3     Fatal Errors
+        11    Cannot open project file for writing
+        12    Device with given name in not found in database
+        13    Error writing project file
+        15    Error reading import XML file
+        20    Error converting project
+        http://www.keil.com/support/man/docs/uv4/uv4_commandline.htm
+    '''
     syscall("echo compilation...")
     result = syscall("%KEIL_UV_DIR%\\UV4.exe -b scm_v3c\\applications\\freq_sweep_rx\\freq_sweep_rx.uvprojx -o \"output.txt\"")
-    assert result == 0
+    assert result <= 1
     
 def test_bootload():
     syscall("echo bootload SCuM...")
