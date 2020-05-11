5a793fd19764a40b361b1f54f7537db798eb3350
changtengfei <tengfei.chang@gmail.com>
2020-05-11 10:26:47 +0200

    SW-25. select the target coarse mid settings, and only turn fine code later according to IF estimate

--- scm_v3c/applications/if_estimate_test/if_estimate_test.c
+++ scm_v3c/applications/if_estimate_test/if_estimate_test.c
@@ -33,6 +33,9 @@ packet is received and adjust the coarse, mid and fine settings accordingly.
 #define NUMPKT_PER_CFG      1
 #define STEPS_PER_CONFIG    32
 
+#define COARSE_START        23
+#define COARSE_END          25
+
 //=========================== variables =======================================
 
 typedef struct {
@@ -54,6 +57,13 @@ typedef struct {
                 uint8_t         cfg_coarse;
                 uint8_t         cfg_mid;
                 uint8_t         cfg_fine;
+    
+                uint8_t         target_coarse;
+                uint8_t         target_mid;
+                
+                uint16_t        settings[STEPS_PER_CONFIG];
+                uint8_t         num_settings;
+                uint8_t         pre_num_settings;
 } app_vars_t;
 
 app_vars_t app_vars;
@@ -75,7 +85,6 @@ int main(void) {
     uint8_t         offset;
     
     memset(&app_vars,0,sizeof(app_vars_t));
-
     
     printf("Initializing...");
         
@@ -135,7 +144,7 @@ int main(void) {
     while(1){
         
         // loop through all configuration
-        for (app_vars.cfg_coarse=0;app_vars.cfg_coarse<STEPS_PER_CONFIG;app_vars.cfg_coarse++){
+        for (app_vars.cfg_coarse=COARSE_START;app_vars.cfg_coarse<COARSE_END;app_vars.cfg_coarse++){
             for (app_vars.cfg_mid=0;app_vars.cfg_mid<STEPS_PER_CONFIG;app_vars.cfg_mid++){
                 for (app_vars.cfg_fine=0;app_vars.cfg_fine<STEPS_PER_CONFIG;app_vars.cfg_fine++){
 //                    printf(
@@ -155,6 +164,14 @@ int main(void) {
                 }
             }
         }
+        
+        printf("Sweep Done!\r\n");
+        printf(
+            "target_coarse = %d target_mid = %d (num_settings = %d)\r\n", 
+            app_vars.target_coarse, 
+            app_vars.target_mid,
+            app_vars.pre_num_settings
+        );
     }
 }
 
@@ -169,7 +186,8 @@ void    cb_startFrame_rx(uint32_t timestamp){
 
 void    cb_endFrame_rx(uint32_t timestamp){
     
-    uint8_t i;
+    uint8_t  i;
+    uint16_t setting;
     
     radio_getReceivedFrame(
         &(app_vars.packet[0]),
@@ -179,17 +197,18 @@ void    cb_endFrame_rx(uint32_t timestamp){
         &app_vars.rxpk_lqi
     );
         
+    // Only record IF estimate, LQI, and CDR tau for valid packets
+    app_vars.IF_estimate        = radio_getIFestimate();
+    app_vars.LQI_chip_errors    = radio_getLQIchipErrors();
+        
     radio_rfOff();
     
     if(
         app_vars.packet_len == LEN_RX_PKT && (radio_getCrcOk())
     ){
-        // Only record IF estimate, LQI, and CDR tau for valid packets
-        app_vars.IF_estimate        = radio_getIFestimate();
-        app_vars.LQI_chip_errors    = radio_getLQIchipErrors();
         
         printf(
-            "pkt received on ch%d %c%c%c%c.%d.%d.%d\r\n",
+            "pkt received on ch%d %c%c%c%c.%d.%d.%d IF_estimate %d LQI_chip_errors %d \r\n",
             app_vars.packet[4],
             app_vars.packet[0],
             app_vars.packet[1],
@@ -197,11 +216,61 @@ void    cb_endFrame_rx(uint32_t timestamp){
             app_vars.packet[3],
             app_vars.cfg_coarse,
             app_vars.cfg_mid,
-            app_vars.cfg_fine
+            app_vars.cfg_fine,
+            app_vars.IF_estimate,
+            app_vars.LQI_chip_errors
         );
         
         app_vars.packet_len = 0;
         memset(&app_vars.packet[0],0,LENGTH_PACKET);
+        
+        // record the settings
+        
+        setting = (app_vars.cfg_coarse << 10) | \
+                  (app_vars.cfg_mid    << 5)  | \
+                  (app_vars.cfg_fine);
+        
+        if (
+            app_vars.target_coarse == 0 ||
+            app_vars.target_mid    == 0
+        ){
+            
+            // first time record
+            app_vars.num_settings     = 0;
+            
+            app_vars.settings[0]      = setting;
+            
+            app_vars.target_coarse    = (app_vars.settings[0] >> 10) & 0x001F;
+            app_vars.target_mid       = (app_vars.settings[0] >> 5 ) & 0x001F;
+            
+            app_vars.num_settings += 1;
+            
+        } else {
+            if (
+                ((app_vars.settings[0] >> 10) & 0x001F) == app_vars.cfg_coarse &&
+                ((app_vars.settings[0] >> 5 ) & 0x001F) == app_vars.cfg_mid
+            ) {
+                app_vars.settings[app_vars.num_settings] = setting;
+                
+                app_vars.num_settings += 1;
+            } else {
+                
+                // new coarse and mid settings
+                
+                if (app_vars.num_settings>app_vars.pre_num_settings){
+                    app_vars.pre_num_settings = app_vars.num_settings;
+                    app_vars.target_coarse    = (app_vars.settings[0] >> 10) & 0x001F;
+                    app_vars.target_mid       = (app_vars.settings[0] >> 5 ) & 0x001F;
+                }
+                
+                // first time record
+                app_vars.num_settings     = 0;
+                app_vars.settings[0]      = setting;
+                app_vars.num_settings    += 1;
+            }
+        }
+        
+        
     }
     
     radio_rxEnable();
