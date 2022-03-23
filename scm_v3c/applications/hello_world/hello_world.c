8a5f01690b272df0fb82e855508d19a02f58204e
Austin Patel <austinapatel@gmail.com>
2022-03-22 20:14:57 -0700

    Add Hello World example app

---
+++ scm_v3c/applications/hello_world/hello_world.c
@@ -0,0 +1,71 @@
+/**
+\brief This program calibrates, then continuously prints out a hello world message.
+*/
+
+#include <string.h>
+
+#include "scm3c_hw_interface.h"
+#include "memory_map.h"
+#include "optical.h"
+
+//=========================== defines =========================================
+
+#define CRC_VALUE         (*((unsigned int *) 0x0000FFFC))
+#define CODE_LENGTH       (*((unsigned int *) 0x0000FFF8))
+
+//=========================== variables =======================================
+
+typedef struct {
+                uint8_t          count;
+} app_vars_t;
+
+app_vars_t app_vars;
+
+//=========================== prototypes ======================================
+
+//=========================== main ============================================
+
+int main(void) {
+    uint32_t calc_crc;
+	  uint32_t i;
+
+    memset(&app_vars,0,sizeof(app_vars_t));
+    
+    printf("Initializing...");
+        
+    // Set up mote configuration
+    // This function handles all the analog scan chain setup
+    initialize_mote();
+      
+    // Check CRC to ensure there were no errors during optical programming
+    printf("\r\n-------------------\r\n");
+    printf("Validating program integrity..."); 
+    
+    calc_crc = crc32c(0x0000,CODE_LENGTH);
+    
+    if(calc_crc == CRC_VALUE){
+        printf("CRC OK\r\n");
+    } else{
+        printf("\r\nProgramming Error - CRC DOES NOT MATCH - Halting Execution\r\n");
+        while(1);
+    }
+    
+    // Enable optical SFD interrupt for optical calibration
+    optical_enable();
+    
+    // Wait for optical cal to finish
+    while(optical_getCalibrationFinshed() == 0);
+
+    printf("Cal complete\r\n");
+    
+    while(1){
+        printf("Hello World! %d\n", app_vars.count);
+				app_vars.count += 1;
+			
+        for (i = 0; i < 1000000; i++);			
+    }
+}
+
+//=========================== public ==========================================
+
+//=========================== private =========================================
