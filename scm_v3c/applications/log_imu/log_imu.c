23ca0af6ced05b8b5b4576c8c1c4aa03a0eb36ee
Austin Patel <austinapatel@gmail.com>
2022-03-26 23:51:31 -0700

    Cleanup hello_world and log_imu examples

--- scm_v3c/applications/log_imu/log_imu.c
+++ scm_v3c/applications/log_imu/log_imu.c
@@ -23,37 +23,13 @@ app_vars_t app_vars;
 //=========================== main ============================================
 
 int main(void) {
-    uint32_t calc_crc;
-	  uint32_t i;
+    uint32_t i;
 
     memset(&app_vars,0,sizeof(app_vars_t));
     
-    printf("Initializing...");
-        
-    // Set up mote configuration
-    // This function handles all the analog scan chain setup
     initialize_mote();
-      
-    // Check CRC to ensure there were no errors during optical programming
-    printf("\r\n-------------------\r\n");
-    printf("Validating program integrity..."); 
-    
-    calc_crc = crc32c(0x0000,CODE_LENGTH);
-    
-    if(calc_crc == CRC_VALUE){
-        printf("CRC OK\r\n");
-    } else{
-        printf("\r\nProgramming Error - CRC DOES NOT MATCH - Halting Execution\r\n");
-        while(1);
-    }
-    
-    // Enable optical SFD interrupt for optical calibration
-    optical_enable();
-    
-    // Wait for optical cal to finish
-    while(optical_getCalibrationFinshed() == 0);
-
-    printf("Cal complete\r\n");
+    crc_check();
+    perform_calibration();
     
     // Configure GPIO
     // Set GPI enables
@@ -75,7 +51,7 @@ int main(void) {
         read_all_imu_data(&app_vars.imu_measurement);
         log_imu_data(&app_vars.imu_measurement);
 
-        for (i = 0; i < 100000; i++);			
+        for (i = 0; i < 100000; i++);
     }
 }
 
