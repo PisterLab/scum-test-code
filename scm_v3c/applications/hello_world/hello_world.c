ed8b401a10bcf58397705a453ea4b12ce0cd02c0
Austin Patel <austinapatel@gmail.com>
2022-03-26 23:52:07 -0700

    Cleanup hello_world and log_imu examples (#29)

--- scm_v3c/applications/hello_world/hello_world.c
+++ scm_v3c/applications/hello_world/hello_world.c
@@ -12,7 +12,7 @@
 //=========================== variables =======================================
 
 typedef struct {
-                uint8_t          count;
+    uint8_t count;
 } app_vars_t;
 
 app_vars_t app_vars;
@@ -22,43 +22,21 @@ app_vars_t app_vars;
 //=========================== main ============================================
 
 int main(void) {
-    uint32_t calc_crc;
-	  uint32_t i;
+    uint32_t i;
 
     memset(&app_vars,0,sizeof(app_vars_t));
     
     printf("Initializing...");
         
-    // Set up mote configuration
-    // This function handles all the analog scan chain setup
     initialize_mote();
-      
-    // Check CRC to ensure there were no errors during optical programming
-    printf("\r\n-------------------\r\n");
-    printf("Validating program integrity..."); 
-    
-    calc_crc = crc32c(0x0000,CODE_LENGTH);
-    
-    if(calc_crc == CRC_VALUE){
-        printf("CRC OK\r\n");
-    } else{
-        printf("\r\nProgramming Error - CRC DOES NOT MATCH - Halting Execution\r\n");
-        while(1);
-    }
-    
-    // Enable optical SFD interrupt for optical calibration
-    optical_enable();
-    
-    // Wait for optical cal to finish
-    while(optical_getCalibrationFinshed() == 0);
-
-    printf("Cal complete\r\n");
+    crc_check();
+    perform_calibration();
     
     while(1){
         printf("Hello World! %d\n", app_vars.count);
         app_vars.count += 1;
-			
-        for (i = 0; i < 1000000; i++);			
+        
+        for (i = 0; i < 1000000; i++);
     }
 }
 
