e5302d8930f37af3b2dee11d686c26b1654b2546
Jacob <72148836+JacobLouie@users.noreply.github.com>
2023-04-06 14:09:43 -0700

    freq_sweep_tx_simple Fixed + Calibration Fix
    Made changes to repeat_rx_tx() and LC_FEQCHANGE() function to fix simple sweep project. Added dummy variable to optical_sfd_isr() function that fixes the chip calibration.

--- scm_v3c/radio.c
+++ scm_v3c/radio.c
@@ -226,6 +226,10 @@ void repeat_rx_tx(repeat_rx_tx_params_t repeat_rx_tx_params) {
     uint8_t cfg_coarse_stop;
     uint8_t cfg_mid_stop;
     uint8_t cfg_fine_stop;
+		
+		// 1.1V (helps reorder assembly code)
+		uint8_t *txPacket_fix = repeat_rx_tx_params.txPacket;
+		uint8_t pkt_len_fix = repeat_rx_tx_params.pkt_len;
 
     int pkt_count = 0;
     char* radio_mode_string;
@@ -258,11 +262,14 @@ void repeat_rx_tx(repeat_rx_tx_params_t repeat_rx_tx_params) {
         cfg_mid_stop = repeat_rx_tx_params.sweep_lc_mid_end;
         cfg_fine_start = repeat_rx_tx_params.sweep_lc_fine_start;
         cfg_fine_stop = repeat_rx_tx_params.sweep_lc_fine_end;
-        printf("Sweeping %s from Coarse: %d-%d  Mid: %d-%d  Fine: %d-%d\n",
-               radio_mode_string, cfg_coarse_start, cfg_coarse_stop,
-               cfg_mid_start, cfg_mid_stop, cfg_fine_start, cfg_fine_stop);
+        // 1.1V probably can be added back in if broken down
+				//printf("Sweeping %s from Coarse: %d-%d  Mid: %d-%d  Fine: %d-%d\n",
+        //       radio_mode_string, cfg_coarse_start, cfg_coarse_stop,
+        //       cfg_mid_start, cfg_mid_stop, cfg_fine_start, cfg_fine_stop);
     }
-
+		// 1.1V
+		__asm("NOP");
+		
     while (1) {
         // loop through all LC configuration
         for (cfg_coarse = cfg_coarse_start; cfg_coarse < cfg_coarse_stop;
@@ -290,12 +297,14 @@ void repeat_rx_tx(repeat_rx_tx_params_t repeat_rx_tx_params) {
                             repeat_rx_tx_params.txPacket[i] = ' ';
                         }
 
+                        // 1.1V
+												txPacket_fix = repeat_rx_tx_params.txPacket;
+												__asm("NOP");
+												// 1.1V
                         repeat_rx_tx_params.fill_tx_packet(
-                            repeat_rx_tx_params.txPacket,
-                            repeat_rx_tx_params.pkt_len, state);
-
-                        send_packet(repeat_rx_tx_params.txPacket,
-                                    repeat_rx_tx_params.pkt_len);
+                            txPacket_fix, pkt_len_fix, state);
+												// 1.1V
+                        send_packet(txPacket_fix, pkt_len_fix);
                     }
 
                     pkt_count += 1;
--- scm_v3c/scm3c_hw_interface.c
+++ scm_v3c/scm3c_hw_interface.c
@@ -1476,9 +1476,13 @@ void LC_FREQCHANGE(int coarse, int mid, int fine) {
     //        none, it programs the LC radio frequency immediately
 
     // mask to ensure that the coarse, mid, and fine are actually 5-bit
+		// 1.1V (NOP)
     char coarse_m = (char)(coarse & 0x1F);
+		__asm("NOP");
     char mid_m = (char)(mid & 0x1F);
-    char fine_m = (char)(fine & 0x1F);
+		__asm("NOP");
+		char fine_m = (char)(fine & 0x1F);
+		__asm("NOP");
 
     // flip the bit order to make it fit more easily into the ACFG registers
     unsigned int coarse_f = (unsigned int)(flipChar(coarse_m));
