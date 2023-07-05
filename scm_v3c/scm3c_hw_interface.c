c18b3c6560dce894f0cf6790fa4ba8441e1660a0
Jacob <72148836+JacobLouie@users.noreply.github.com>
2023-07-05 15:55:06 -0700

    fix-lint (#63)

--- scm_v3c/scm3c_hw_interface.c
+++ scm_v3c/scm3c_hw_interface.c
@@ -1441,7 +1441,7 @@ void set_asc_bit(unsigned int position) {
 
     scm3c_hw_interface_vars.ASC[index] |=
         0x80000000 >> (position - (index << 5));
-    
+
     // Needed for no 1.1V -> VDDD fix
     __asm("NOP");
 
@@ -1476,13 +1476,13 @@ void LC_FREQCHANGE(int coarse, int mid, int fine) {
     //        none, it programs the LC radio frequency immediately
 
     // mask to ensure that the coarse, mid, and fine are actually 5-bit
-		// 1.1V (NOP)
+    // 1.1V (NOP)
     char coarse_m = (char)(coarse & 0x1F);
-		__asm("NOP");
+    __asm("NOP");
     char mid_m = (char)(mid & 0x1F);
-		__asm("NOP");
-		char fine_m = (char)(fine & 0x1F);
-		__asm("NOP");
+    __asm("NOP");
+    char fine_m = (char)(fine & 0x1F);
+    __asm("NOP");
 
     // flip the bit order to make it fit more easily into the ACFG registers
     unsigned int coarse_f = (unsigned int)(flipChar(coarse_m));
