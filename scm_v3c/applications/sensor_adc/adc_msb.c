289ca333c278d3fa05e849b12bdf7cb6674dc3fa
Titan Yuan <tryuan99@gmail.com>
2023-09-01 10:34:37 -0700

    Limit ADC sample to 10 bits

--- scm_v3c/applications/sensor_adc/adc_msb.c
+++ scm_v3c/applications/sensor_adc/adc_msb.c
@@ -27,6 +27,11 @@ bool adc_msb_disambiguate(const uint16_t adc_sample) {
             g_adc_msb_previous_sample =
                 adc_sample + (num_multiples + 1) * (ADC_MAX_ADC_SAMPLE + 1);
         }
+
+        // Limit the ADC sample to 10 bits.
+        if (g_adc_msb_previous_sample > ADC_MAX_THEORETICAL_ADC_SAMPLE) {
+            g_adc_msb_previous_sample -= (ADC_MAX_ADC_SAMPLE + 1);
+        }
     } else {
         g_adc_msb_previous_sample = adc_sample;
     }
