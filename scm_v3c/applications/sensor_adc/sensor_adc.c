ea5e19d91dc3f4da6ed20c5a25f9d0a4900ac0e6
Titan Yuan <tryuan99@gmail.com>
2023-10-09 19:47:50 -0700

    Excite the sensor with a GPIO

--- scm_v3c/applications/sensor_adc/sensor_adc.c
+++ scm_v3c/applications/sensor_adc/sensor_adc.c
@@ -26,16 +26,71 @@ static const adc_config_t g_adc_config = {
     .pga_bypass = true,
 };
 
+// GPIO configuration.
+static uint16_t g_gpio_input_enable = 0x0000;
+static uint16_t g_gpio_output_enable = 0x0000;
+
+// GPIO 0 is the excitation pin.
+static const uint8_t g_gpio_excitation = 0;
+
+// Empty callback for the RF timer.
+static void rftimer_empty_callback(void) {}
+
+// Callback for the RF timer.
 static void rftimer_callback(void) {
     // Trigger an ADC read.
     adc_trigger();
     delay_milliseconds_asynchronous(/*delay_milli=*/10, 7);
 }
 
+// Set the GPIO input and output enables in the analog scan chain.
+static inline void gpio_set_enable(void) {
+    GPI_enables(g_gpio_input_enable);
+    GPO_enables(g_gpio_output_enable);
+    analog_scan_chain_write();
+    analog_scan_chain_load();
+}
+
+// Set the GPIO to high.
+static inline void gpio_set_high(const uint8_t gpio) {
+    GPIO_REG__OUTPUT &= ~(1 << gpio);
+    g_gpio_input_enable &= ~(1 << gpio);
+    g_gpio_output_enable |= (1 << gpio);
+    gpio_set_enable();
+    GPIO_REG__OUTPUT |= (1 << gpio);
+}
+
+// Set the GPIO to low.
+static inline void gpio_set_low(const uint8_t gpio) {
+    GPIO_REG__OUTPUT &= ~(1 << gpio);
+    g_gpio_input_enable &= ~(1 << gpio);
+    g_gpio_output_enable |= (1 << gpio);
+    gpio_set_enable();
+    GPIO_REG__OUTPUT &= ~(1 << gpio);
+}
+
+// Set the GPIO to high-Z.
+static inline void gpio_set_high_z(const uint8_t gpio) {
+    // High-Z mode is achieved by enabling the GPIO as an input pin and
+    // outputting a 1 to the GPIO.
+    g_gpio_input_enable |= (1 << gpio);
+    g_gpio_output_enable &= ~(1 << gpio);
+    gpio_set_enable();
+    GPIO_REG__OUTPUT |= (1 << gpio);
+}
+
+// Set the GPIO high for some time before setting it to high-Z.
+static inline void gpio_excite(const uint8_t gpio) {
+    gpio_set_high(gpio);
+    delay_milliseconds_synchronous(/*delay_milli=*/1000, 6);
+    gpio_set_high_z(gpio);
+}
+
 int main(void) {
     initialize_mote();
 
     // Configure the RF timer.
+    rftimer_set_callback_by_id(rftimer_empty_callback, 6);
     rftimer_set_callback_by_id(rftimer_callback, 7);
     rftimer_enable_interrupts();
     rftimer_enable_interrupts_by_id(7);
@@ -51,21 +106,30 @@ int main(void) {
     crc_check();
     perform_calibration();
 
-    // TODO(titan): Set up GPIOs to drive the switch.
+    GPO_control(6, 6, 6, 6);
+    GPI_enables(g_gpio_input_enable);
+    GPO_enables(g_gpio_output_enable);
+
+    analog_scan_chain_write();
+    analog_scan_chain_load();
 
     time_constant_init();
+    gpio_excite(g_gpio_excitation);
     delay_milliseconds_asynchronous(TIME_CONSTANT_SAMPLING_PERIOD_MS, 7);
     while (true) {
         if (g_adc_output.valid) {
             time_constant_add_sample(g_adc_output.data);
             g_adc_output.valid = false;
             if (time_constant_has_sufficient_samples()) {
-                delay_milliseconds_asynchronous(TIME_CONSTANT_MEASUREMENT_PERIOD_MS, 7);
+                delay_milliseconds_asynchronous(
+                    TIME_CONSTANT_MEASUREMENT_PERIOD_MS, 7);
                 printf("Received sufficient samples.\n");
-                const fixed_point_t estimated_time_constant = time_constant_estimate();
+                const fixed_point_t estimated_time_constant =
+                    time_constant_estimate();
                 printf("Estimated time constant: %lld / %lld\n",
                        estimated_time_constant, fixed_point_init(1));
                 time_constant_init();
+                gpio_excite(g_gpio_excitation);
             }
         }
     }
