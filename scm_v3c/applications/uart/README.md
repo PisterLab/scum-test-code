f4f761366d0f3cc85bd1232bbcbc188e9581007a
Titan Yuan <tryuan99@gmail.com>
2024-06-24 12:26:28 -0700

    Add UART project

---
+++ scm_v3c/applications/uart/README.md
@@ -0,0 +1,3 @@
+# uart
+
+Repeatedly transmited a hello world message over UART while echoing back any data received over UART.
---
+++ scm_v3c/applications/uart/uart_main.c
@@ -0,0 +1,35 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "optical.h"
+#include "scm3c_hw_interface.h"
+#include "uart.h"
+
+// Number of for loop cycles between TX messages.
+// 700000 for loop cycles roughly correspond to 1 second.
+#define NUM_CYCLES_BETWEEN_TX 1000000
+
+// TX counter.
+uint32_t g_tx_counter = 0;
+
+// UART RX callback function.
+void uart_rx_callback(const char data) { printf("%c", data); }
+
+int main(void) {
+    initialize_mote();
+
+    // Configure the UART.
+    uart_set_rx_callback(uart_rx_callback);
+    uart_enable_interrupt();
+
+    crc_check();
+    perform_calibration();
+
+    while (true) {
+        printf("Hello World! %u\n", g_tx_counter);
+        ++g_tx_counter;
+
+        for (size_t i = 0; i < NUM_CYCLES_BETWEEN_TX; ++i) {}
+    }
+}
