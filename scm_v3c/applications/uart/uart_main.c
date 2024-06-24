ef87d5f12763eacaea827262012bf7e931b0b4f2
Titan Yuan <tryuan99@gmail.com>
2024-06-24 12:45:36 -0700

    Remove Hello World from UART project

--- scm_v3c/applications/uart/uart_main.c
+++ scm_v3c/applications/uart/uart_main.c
@@ -1,18 +1,9 @@
 #include <stdbool.h>
-#include <stdint.h>
 #include <stdio.h>
-#include <stdlib.h>
 
 #include "optical.h"
 #include "scm3c_hw_interface.h"
 #include "uart.h"
-
-// Number of for loop cycles between TX messages.
-// 700000 for loop cycles roughly correspond to 1 second.
-#define NUM_CYCLES_BETWEEN_TX 1000000
-
-// TX counter.
-uint32_t g_tx_counter = 0;
 
 // UART RX callback function.
 void uart_rx_callback(const char data) { printf("%c", data); }
@@ -27,10 +18,8 @@ int main(void) {
     crc_check();
     perform_calibration();
 
+    printf("Waiting for UART data...\n");
     while (true) {
-        printf("Hello World! %u\n", g_tx_counter);
-        ++g_tx_counter;
-
-        for (size_t i = 0; i < NUM_CYCLES_BETWEEN_TX; ++i) {}
+        __wfi();
     }
 }
